3fafc773c669ac7a0f4433cb26a6073a9279ae0f
GabriHR <ghernrod@myuax.com>
2024-02-21 20:53:02 +0100

    Clase Main emplementada para ejecutar el codigo ya definidio en la clase Recursos_y_Suministros. A su vez se importa las librerias necesarias.

--- Mision_Espacial/src/main/java/org/example/Main.java
+++ Mision_Espacial/src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
@@ -12,5 +13,23 @@ public class Main {
         System.out.println("Tiempo en la Tierra: " + planeta.formatearTiempo(tiempoTerrestre));
         System.out.println("Tiempo en el nuevo planeta: " + planeta.formatearTiempo(tiempoPlaneta));
 
+        //Recursos_y_Suministros
+        double[] recursos = {10.0, 20.0, 30.0};
+        double umbralSeguro = 15.0;
+        Recursos_y_Suministros recursosObj = new Recursos_y_Suministros(recursos, umbralSeguro);
+        recursosObj.imprimirValores();
+        double[] consumo = recursosObj.calcularConsumo(7);
+        System.out.println("Consumo para 7 días: " + Arrays.toString(consumo));
+        double[] proyeccion = recursosObj.proyectarConsumoFuturo(30);
+        System.out.println("Proyección para 30 días: " + Arrays.toString(proyeccion));
+        boolean alerta = recursosObj.alertaUmbralSeguro();
+        System.out.println("Alerta de umbral seguro: " + alerta);
+        double[] estadisticas = recursosObj.estadisticasUso();
+        System.out.println("Estadísticas de uso: Promedio = " + estadisticas[0] + ", Mínimo = " + estadisticas[1] + ", Máximo = " + estadisticas[2]);
+
     }
 }
+
+
+
+
