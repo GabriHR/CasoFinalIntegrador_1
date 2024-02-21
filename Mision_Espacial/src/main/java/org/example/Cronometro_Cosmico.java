a1a60bd3a1c737ace3c5dfe9923d2da9cc695101
GabriHR <ghernrod@myuax.com>
2024-02-21 20:36:48 +0100

    public class Cronometro_Cosmico en la que se definen las variables necesarias en este caso de tipo double y se recoge el printeo de dichos valores, ya transformados.

--- Mision_Espacial/src/main/java/org/example/Cronometro_Cosmico.java
+++ Mision_Espacial/src/main/java/org/example/Cronometro_Cosmico.java
@@ -1,17 +1,18 @@
 package org.example;
 public class Cronometro_Cosmico {
     static final double DURACION_DIA = 30.0;
-
+    //Convierte el tiempo terrestre a tiempo en el nuevo planeta
     public double convertirTiempo(double tiempoTerrestre) {
         return tiempoTerrestre / DURACION_DIA * 24.0;
     }
+    //Formatea el tiempo en segundos, minutos, horas, días y años
     public String formatearTiempo(double tiempo) {
         double segundos = tiempo * 24 * 60 * 60;
         double minutos = tiempo * 24 * 60;
         double horas = tiempo * 24;
         double dias = tiempo;
         double anos = tiempo / 365.25;
-
+        //Devuelve el tiempo formateado
         return String.format("Segundos: %.2f, Minutos: %.2f, Horas: %.2f, Días: %.2f, Años: %.2f",
                 segundos, minutos, horas, dias,anos);
     }
